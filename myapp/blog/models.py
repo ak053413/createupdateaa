6305de79b29f8d60fd296a8d81451a01ee30ab2f
Ashish Kumar <ak053413@gmail.com>
2023-08-14 07:03:36 +0530

    some changes

--- myapp/blog/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(null=True, blank=True)
-    propic = models.ImageField(upload_to='pictures/', max_length=255, null=True, blank=True)
--- myapp/forms.py
+++ myapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm, UsernameField,UserChangeForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm, UsernameField, UserChangeForm
 from django.contrib.auth.models import User
 from django.utils.translation import gettext, gettext_lazy as _
 from .models import Profile
